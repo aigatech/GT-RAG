1f57f297bcaa5b574f58a51820eff8593278edec
Naman Talreja <namantalreja2704@gmail.com>
2024-10-21 14:29:45 -0400

    Added niche scraper

---
+++ niche.py
@@ -0,0 +1,21 @@
+from firecrawl import FirecrawlApp
+
+app = FirecrawlApp(api_key='ADD_API_KEY')
+
+crawl_result = app.crawl_url('https://www.niche.com/colleges/georgia-institute-of-technology/', 
+                            params={ 
+                                'limit': 2, #EDIT LIMIT HERE
+                                'scrapeOptions': {'formats': [ 'html' ]}
+	                           
+                                
+                            },
+                            poll_interval=30
+                            
+                        )
+
+for index, page in enumerate(crawl_result['data']):
+    output_file = f'crawl_result_page_{index + 1}.txt' 
+    with open(output_file, 'w') as file:
+        file.write(page['html']) 
+
+    print(f'Page {index + 1} has been written to {output_file}')
