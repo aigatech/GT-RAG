94bc1ab4efe4a857b85d65ffe09d9e812414f6c4
Naman Talreja <namantalreja2704@gmail.com>
2024-10-23 01:51:18 -0400

    added config.json

--- src/scrapers/run_spiders.py
+++ src/scrapers/run_spiders.py
@@ -2,8 +2,10 @@ import json
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from scrapers.spiders.singlesite_spider import SingleSiteSpider
+import os 
+config_path = os.path.join(os.path.dirname(__file__), 'config.json')
 
-with open('config.json', 'r') as f:
+with open(config_path, 'r') as f:
     config = json.load(f)
     urls = config.get('urls', [])
 
